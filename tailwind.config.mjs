b670b1ea9289642af8a96b900b162a2a27a62b07
Gonzalo Chale <miguelgchale@gmail.com>
2024-07-13 16:07:40 -0500

    project setup

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "",
+        card: "",
+        foreground: "",
+        "muted-foreground": "",
+        border: "",
+      },
+    },
+  },
+  plugins: [],
+};
