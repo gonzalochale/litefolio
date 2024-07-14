754cb0405163b634f75b22dff4527d6b9486dca7
Gonzalo Chale <miguelgchale@gmail.com>
2024-07-14 00:04:24 -0500

    add:home page components (hero and stack)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,49 @@
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      animation: {
+        "slide-logos": "slide-logos 40s linear infinite",
+        aurora: "aurora 60s linear infinite",
+      },
+      keyframes: {
+        "slide-logos": {
+          to: { transform: "translateX(calc(-50% - 2rem))" },
+        },
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
+      },
       colors: {
-        background: "",
-        card: "",
-        foreground: "",
-        "muted-foreground": "",
-        border: "",
+        background: "#FAFAFA",
+        card: "#FFFFFF",
+        foreground: "#171717",
+        "muted-foreground": "#666666",
+        border: "#EBEBEB",
+        accent: "#F2F2F2",
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
