c46f00da96b70c62dc1835bf5aa469ce8df68430
Gonzalo Chale <miguelgchale@gmail.com>
2024-08-03 18:20:32 -0500

    chore:setup analitycs using plausible instead of vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,10 +7,4 @@ export default defineConfig({
     enabled: false,
   },
   integrations: [tailwind()],
-  output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
 });
